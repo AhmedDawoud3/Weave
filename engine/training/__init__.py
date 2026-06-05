c167acaa8110375418c107d6b62a96b728c86874
Ahmed Dawoud <68483546+AhmedDawoud3@users.noreply.github.com>
2026-06-05 15:56:44 +0300

    feat: implement scheduler factory and configure training module documentation

--- engine/training/__init__.py
+++ engine/training/__init__.py
@@ -1,20 +1,3 @@
 from .scheduler_factory import create_scheduler
-from .metrics import compute_batch_metrics, EpochMetricsTracker
-from .callbacks import EarlyStopping, Checkpointing
-from .trainer import Trainer
-from .runner import TrainingRunner
-from .experiments import save_run, get_run, list_runs, delete_run
 
-__all__ = [
-    "create_scheduler",
-    "compute_batch_metrics",
-    "EpochMetricsTracker",
-    "EarlyStopping",
-    "Checkpointing",
-    "Trainer",
-    "TrainingRunner",
-    "save_run",
-    "get_run",
-    "list_runs",
-    "delete_run",
-]
+__all__ = ["create_scheduler"]
